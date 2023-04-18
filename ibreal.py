c17ffb5d17c0d70eb57330922a09ce379a1298f7
Dan Leonardson <oscarpoppa@protonmail.com>
2023-04-17 22:41:41 -0500

    docstring

--- ibreal.py
+++ ibreal.py
@@ -11,8 +11,8 @@ class IBReal:
     Usage:
     realnum = IBReal(raw, prec)
 
-    raw: Ival object, ascii real number in decimal notation or tuple (integer, offset), where integer is the integer after multiplying the real
-         number by 10^offset.
+    raw: Ival object, ascii real number in decimal notation or tuple (integer, offset), where integer is the integer 
+         after multiplying the real number by 10^offset.
 
     prec: precision -- length limit of digits to right of decimal
 
