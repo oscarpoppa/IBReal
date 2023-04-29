86e1dfad8a1ee1eb257a6b71e618bf767a0bf82b
Dan Leonardson <oscarpoppa@protonmail.com>
2023-04-29 12:58:41 -0500

    added set_global_prec func

--- ibfuncs.py
+++ ibfuncs.py
@@ -1,7 +1,11 @@
 from collections import namedtuple
 from functools import wraps
+from os import environ
 
 Memo = namedtuple('Memo','ival prec trim_on')
+
+def set_global_prec(num):
+    environ['IBR_DEF_PREC'] = str(num)
 
 class MemoizeIBRCall:
     def __init__(self):
