b7a19aaed5eb8ecbfce7bd23e689908b0926cf8f
Dan Leonardson <oscarpoppa@protonmail.com>
2023-05-02 16:38:36 -0500

    created proper funcs instead of one-liners for memoized callables

--- ibfuncs.py
+++ ibfuncs.py
@@ -139,8 +139,9 @@ class IBArcTan:
         return rsum
 
 ibarctanmemo = MemoizeIBRCall()
-# memoized callable
-ib_arctan = ibarctanmemo(IBArcTan())
+@ibarctanmemo
+def ib_arctan(*args, **kwargs):
+    return IBArcTan()(*args, **kwargs)
 
 class IBExp:
     def __call__(self, val):
@@ -184,8 +185,9 @@ class IBExp:
         return rsum
 
 ibexpmemo = MemoizeIBRCall()
-# memoized callable
-ib_exp = ibexpmemo(IBExp())
+@ibexpmemo
+def ib_exp(*args, **kwargs):
+    return IBExp()(*args, **kwargs)
 
 class IBLog:
     def __init__(self):
@@ -255,8 +257,9 @@ class IBLog:
             return self._log_real(val) - (cnt * self._log2)
 
 iblogmemo = MemoizeIBRCall()
-# memoized callable
-ib_log = iblogmemo(IBLog())
+@iblogmemo
+def ib_log(*args, **kwargs):
+    return IBLog()(*args, **kwargs)
 
 # returns a closure to allow access to any branch 
 # (i.e. ib_logs(C('1+2i'))(3) for third branch)
